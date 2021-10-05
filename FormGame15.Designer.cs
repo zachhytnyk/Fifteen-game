1f104a3fb2218db016ae43c74690bee64b920a09
OLECSII MIR <zachhytnyk@live.com>
2021-10-05 21:38:52 +0300

    Создан Класс Game. Доделана форма.

--- FormGame15.Designer.cs
+++ FormGame15.Designer.cs
@@ -94,6 +94,7 @@ namespace Fifteen_game
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(144, 118);
             this.button15.TabIndex = 15;
+            this.button15.Tag = "15";
             this.button15.Text = "-";
             this.button15.UseVisualStyleBackColor = true;
             this.button15.Click += new System.EventHandler(this.button0_Click);
@@ -107,6 +108,7 @@ namespace Fifteen_game
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(144, 118);
             this.button14.TabIndex = 14;
+            this.button14.Tag = "14";
             this.button14.Text = "-";
             this.button14.UseVisualStyleBackColor = true;
             this.button14.Click += new System.EventHandler(this.button0_Click);
@@ -120,6 +122,7 @@ namespace Fifteen_game
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(144, 118);
             this.button13.TabIndex = 13;
+            this.button13.Tag = "13";
             this.button13.Text = "-";
             this.button13.UseVisualStyleBackColor = true;
             this.button13.Click += new System.EventHandler(this.button0_Click);
@@ -133,6 +136,7 @@ namespace Fifteen_game
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(144, 118);
             this.button12.TabIndex = 12;
+            this.button12.Tag = "12";
             this.button12.Text = "-";
             this.button12.UseVisualStyleBackColor = true;
             this.button12.Click += new System.EventHandler(this.button0_Click);
@@ -146,6 +150,7 @@ namespace Fifteen_game
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(144, 115);
             this.button11.TabIndex = 11;
+            this.button11.Tag = "11";
             this.button11.Text = "-";
             this.button11.UseVisualStyleBackColor = true;
             this.button11.Click += new System.EventHandler(this.button0_Click);
@@ -159,6 +164,7 @@ namespace Fifteen_game
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(144, 115);
             this.button10.TabIndex = 10;
+            this.button10.Tag = "10";
             this.button10.Text = "-";
             this.button10.UseVisualStyleBackColor = true;
             this.button10.Click += new System.EventHandler(this.button0_Click);
@@ -172,6 +178,7 @@ namespace Fifteen_game
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(144, 115);
             this.button9.TabIndex = 9;
+            this.button9.Tag = "9";
             this.button9.Text = "-";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button0_Click);
@@ -185,6 +192,7 @@ namespace Fifteen_game
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(144, 115);
             this.button8.TabIndex = 8;
+            this.button8.Tag = "8";
             this.button8.Text = "-";
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button0_Click);
@@ -198,6 +206,7 @@ namespace Fifteen_game
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(144, 115);
             this.button7.TabIndex = 7;
+            this.button7.Tag = "7";
             this.button7.Text = "-";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button0_Click);
@@ -211,6 +220,7 @@ namespace Fifteen_game
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(144, 115);
             this.button6.TabIndex = 6;
+            this.button6.Tag = "6";
             this.button6.Text = "-";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button0_Click);
@@ -224,6 +234,7 @@ namespace Fifteen_game
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(144, 115);
             this.button5.TabIndex = 5;
+            this.button5.Tag = "5";
             this.button5.Text = "-";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button0_Click);
@@ -237,6 +248,7 @@ namespace Fifteen_game
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(144, 115);
             this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
             this.button4.Text = "-";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button0_Click);
@@ -250,6 +262,7 @@ namespace Fifteen_game
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(144, 115);
             this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
             this.button3.Text = "-";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button0_Click);
@@ -263,6 +276,7 @@ namespace Fifteen_game
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(144, 115);
             this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button0_Click);
@@ -276,6 +290,7 @@ namespace Fifteen_game
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 115);
             this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
             this.button1.Text = "-";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button0_Click);
@@ -289,6 +304,7 @@ namespace Fifteen_game
             this.button0.Name = "button0";
             this.button0.Size = new System.Drawing.Size(144, 115);
             this.button0.TabIndex = 0;
+            this.button0.Tag = "0";
             this.button0.Text = "-";
             this.button0.UseVisualStyleBackColor = true;
             this.button0.Click += new System.EventHandler(this.button0_Click);
