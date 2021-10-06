edf7f480309467cbad7532acfe991dbfb2762fa1
OLEKSII MIR <jzachhytnyk@outlook.com>
2021-10-06 14:30:12 +0300

    Test

--- FormGame15.cs
+++ FormGame15.cs
@@ -17,5 +17,42 @@ namespace Fifteen_game
             InitializeComponent();
         }
 
+        private void button0_Click(object sender, EventArgs e)
+        {
+            int position=Convert.ToInt16(((Button)sender).Tag);
+            button(position).Text = position.ToString();
+        }
+
+        private Button button(int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+        }
+
     }
 }
+
+            
+
+
+
+
+
---
+++ Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fifteen_game
+{
+    class Game
+    {
+    }
+}
